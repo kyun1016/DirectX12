c724e28b1d18b000d30c26461a84d691f15e983a
kyun1016 <42373004+kyun1016@users.noreply.github.com>
2025-02-08 14:34:46 +0900

    인스턴싱 완료 중

---
+++ DirectX12/Instancing/Main.hlsl
@@ -0,0 +1,125 @@
+#include "Memory.hlsli"
+// Main 
+struct VertexIn
+{
+    float3 PosL : POSITION;
+    float3 NormalL : NORMAL;
+    float2 TexC : TEXCOORD;
+};
+
+struct VertexOut
+{
+    float4 PosH : SV_POSITION;
+    float3 PosW : POSITION;
+    float3 NormalW : NORMAL;
+    float2 TexC : TEXCOORD;
+    
+    // nointerpolation is used so the index is not interpolated 
+	// across the triangle.
+    nointerpolation uint MatIndex : MATINDEX;
+};
+
+VertexOut VS(VertexIn vin, uint instanceID : SV_InstanceID)
+{
+    VertexOut vout = (VertexOut) 0.0f;
+    
+    InstanceData instData = gInstanceData[instanceID];
+    float4x4 world = instData.World;
+    float4x4 texTransform = instData.TexTransform;
+    float4x4 worldInvTranspose = instData.WorldInvTranspose;
+    float2 displacementMapTexelSize = instData.DisplacementMapTexelSize;
+    float gridSpatialStep = instData.GridSpatialStep;
+    uint matIndex = instData.MaterialIndex;
+    
+    vout.MatIndex = matIndex;
+    
+    // Fetch the material data.
+    MaterialData matData = gMaterialData[matIndex];
+
+#ifdef DISPLACEMENT_MAP
+	// Sample the displacement map using non-transformed [0,1]^2 tex-coords.
+    vin.PosL.y += gDisplacementMap.SampleLevel(gsamLinearWrap, vin.TexC, 1.0f).r;
+	
+	// Estimate normal using finite difference.
+    float du = displacementMapTexelSize.x;
+    float dv = displacementMapTexelSize.y;
+    float l = gDisplacementMap.SampleLevel(gsamPointClamp, vin.TexC - float2(du, 0.0f), 0.0f).r;
+    float r = gDisplacementMap.SampleLevel(gsamPointClamp, vin.TexC + float2(du, 0.0f), 0.0f).r;
+    float t = gDisplacementMap.SampleLevel(gsamPointClamp, vin.TexC - float2(0.0f, dv), 0.0f).r;
+    float b = gDisplacementMap.SampleLevel(gsamPointClamp, vin.TexC + float2(0.0f, dv), 0.0f).r;
+    vin.NormalL = normalize(float3(-r + l, 2.0f * gridSpatialStep, b - t));
+#endif
+    
+    // Transform to world space.
+    float4 posW = mul(float4(vin.PosL, 1.0f), world);
+    vout.PosW = posW.xyz;
+
+    // Assumes nonuniform scaling; otherwise, need to use inverse-transpose of world matrix.
+    vout.NormalW = mul(vin.NormalL, (float3x3) worldInvTranspose);
+
+    // Transform to homogeneous clip space.
+    vout.PosH = mul(posW, gViewProj);
+	
+	// Output vertex attributes for interpolation across triangle.
+    float4 texC = mul(float4(vin.TexC, 0.0f, 1.0f), texTransform);
+    vout.TexC = mul(texC, matData.MatTransform).xy;
+	
+    return vout;
+}
+
+struct PixelOut
+{
+    float4 color0 : SV_Target0;
+    float4 color1 : SV_Target1;
+};
+
+PixelOut PS(VertexOut pin)
+{
+    // Fetch the material data.
+    MaterialData matData = gMaterialData[pin.MatIndex];
+    float4 diffuseAlbedo = matData.DiffuseAlbedo;
+    float3 fresnelR0 = matData.FresnelR0;
+    float roughness = matData.Roughness;
+    uint diffuseTexIndex = matData.DiffuseMapIndex;
+
+    diffuseAlbedo *= gDiffuseMap[diffuseTexIndex].Sample(gsamLinearWrap, pin.TexC);
+	
+#ifdef ALPHA_TEST
+	// Discard pixel if texture alpha < 0.1.  We do this test as soon 
+	// as possible in the shader so that we can potentially exit the
+	// shader early, thereby skipping the rest of the shader code.
+	clip(diffuseAlbedo.a - 0.1f);
+#endif
+
+    // Interpolating normal can unnormalize it, so renormalize it.
+    pin.NormalW = normalize(pin.NormalW);
+
+    // Vector from point being lit to eye. 
+    float3 toEyeW = gEyePosW - pin.PosW;
+    float distToEye = length(toEyeW);
+    toEyeW /= distToEye; // normalize
+
+    // Light terms.
+    float4 ambient = gAmbientLight * diffuseAlbedo;
+
+    const float shininess = 1.0f - roughness;
+    Material mat = { diffuseAlbedo, fresnelR0, shininess };
+    float3 shadowFactor = 1.0f;
+    float4 directLight = ComputeLighting(gLights, mat, pin.PosW,
+        pin.NormalW, toEyeW, shadowFactor);
+
+    float4 litColor = ambient + directLight;
+
+#ifdef FOG
+    float fogAmount = saturate((distToEye - gFogStart) / gFogRange);
+    litColor = lerp(litColor, gFogColor, fogAmount);
+#endif
+
+    // Common convention to take alpha from diffuse albedo.
+    litColor.a = diffuseAlbedo.a;
+
+    PixelOut ret;
+    ret.color0 = litColor;
+    ret.color1 = litColor;
+    return ret;
+}
---
+++ DirectX12/Instancing/Tessellation.hlsl
@@ -0,0 +1,225 @@
+#include "Memory.hlsli"
+
+struct VertexIn
+{
+    float3 PosL : POSITION;
+    float3 NormalL : NORMAL;
+    float2 TexC : TEXCOORD;
+};
+
+struct VertexOut
+{
+    float3 PosL : POSITION;
+    float3 NormalL : NORMAL;
+    float2 TexC : TEXCOORD;
+    
+    // nointerpolation is used so the index is not interpolated 
+	// across the triangle.
+    nointerpolation uint InsIndex : SV_InstanceID;
+};
+
+VertexOut VS(VertexIn vin, uint instanceID : SV_InstanceID)
+{
+    VertexOut vout;
+	
+    vout.PosL = vin.PosL;
+    vout.NormalL = vin.NormalL;
+    vout.TexC = vin.TexC;
+    vout.InsIndex = instanceID;
+    
+    return vout;
+}
+ 
+struct PatchTess
+{
+    float EdgeTess[4] : SV_TessFactor;
+    float InsideTess[2] : SV_InsideTessFactor;
+};
+
+PatchTess ConstantHS(InputPatch<VertexOut, 4> patch, uint patchID : SV_PrimitiveID)
+{
+    PatchTess pt;
+    
+    InstanceData instData = gInstanceData[patch[0].InsIndex];
+    float4x4 world = instData.World;
+    float4x4 texTransform = instData.TexTransform;
+    float4x4 worldInvTranspose = instData.WorldInvTranspose;
+    uint matIndex = instData.MaterialIndex;
+	
+    float3 centerL = 0.25f * (patch[0].PosL + patch[1].PosL + patch[2].PosL + patch[3].PosL);
+    float3 centerW = mul(float4(centerL, 1.0f), world).xyz;
+	
+    float d = distance(centerW, gEyePosW);
+
+	// Tessellate the patch based on distance from the eye such that
+	// the tessellation is 0 if d >= d1 and 64 if d <= d0.  The interval
+	// [d0, d1] defines the range we tessellate in.
+	
+    const float d0 = 20.0f;
+    const float d1 = 100.0f;
+    float tess = 64.0f * saturate((d1 - d) / (d1 - d0));
+
+	// Uniformly tessellate the patch.
+
+    pt.EdgeTess[0] = tess;
+    pt.EdgeTess[1] = tess;
+    pt.EdgeTess[2] = tess;
+    pt.EdgeTess[3] = tess;
+	
+    pt.InsideTess[0] = tess;
+    pt.InsideTess[1] = tess;
+	
+    return pt;
+}
+
+struct HullOut
+{
+    float3 PosL : POSITION;
+    float3 NormalL : NORMAL;
+    float2 TexC : TEXCOORD;
+    
+    // nointerpolation is used so the index is not interpolated 
+	// across the triangle.
+    nointerpolation uint InsIndex : SV_InstanceID;
+};
+
+[domain("quad")]
+[partitioning("integer")]
+[outputtopology("triangle_cw")]
+[outputcontrolpoints(4)]
+[patchconstantfunc("ConstantHS")]
+[maxtessfactor(64.0f)]
+HullOut HS(InputPatch<VertexOut, 4> p,
+           uint i : SV_OutputControlPointID,
+           uint patchId : SV_PrimitiveID)
+{
+    HullOut hout;
+	
+    hout.PosL = p[i].PosL;
+    hout.NormalL = p[i].NormalL;
+    hout.TexC = p[i].TexC;
+    hout.InsIndex = p[i].InsIndex;
+	
+    return hout;
+}
+
+struct DomainOut
+{
+    float4 PosH : SV_POSITION;
+    float3 PosW : POSITION;
+    float3 NormalW : NORMAL;
+    float2 TexC : TEXCOORD;
+    
+    // nointerpolation is used so the index is not interpolated 
+	// across the triangle.
+    nointerpolation uint MatIndex : MATINDEX;
+};
+
+// The domain shader is called for every vertex created by the tessellator.  
+// It is like the vertex shader after tessellation.
+[domain("quad")]
+DomainOut DS(PatchTess patchTess,
+             float2 uv : SV_DomainLocation,
+             const OutputPatch<HullOut, 4> quad)
+{
+    DomainOut dout;
+    
+    InstanceData instData = gInstanceData[quad[0].InsIndex];
+    float4x4 world = instData.World;
+    float4x4 texTransform = instData.TexTransform;
+    float4x4 worldInvTranspose = instData.WorldInvTranspose;
+    float2 displacementMapTexelSize = instData.DisplacementMapTexelSize;
+    float gridSpatialStep = instData.GridSpatialStep;
+    uint matIndex = instData.MaterialIndex;
+    
+    MaterialData matData = gMaterialData[matIndex];
+	
+	// Bilinear interpolation.
+    float3 v1 = lerp(quad[0].PosL, quad[1].PosL, uv.x);
+    float3 v2 = lerp(quad[2].PosL, quad[3].PosL, uv.x);
+    float3 p = lerp(v1, v2, uv.y);
+    
+    // Displacement mapping
+    p.y = 0.3f * (p.z * sin(p.x) + p.x * cos(p.z));
+    
+    float3 n1 = lerp(quad[0].NormalL, quad[1].NormalL, uv.x);
+    float3 n2 = lerp(quad[2].NormalL, quad[3].NormalL, uv.x);
+    float3 n = lerp(n1, n2, uv.y);
+    n.y = 0.3f * (n.z * sin(n.x) + n.x * cos(n.z));
+    
+    float2 t1 = lerp(quad[0].TexC, quad[1].TexC, uv.x);
+    float2 t2 = lerp(quad[2].TexC, quad[3].TexC, uv.x);
+    float2 t = lerp(t1, t2, uv.y);
+	
+	
+    float4 posW = mul(float4(p, 1.0f), world);
+    dout.PosW = posW.xyz;
+    
+    dout.NormalW = mul(n, (float3x3) world);
+    
+    dout.PosH = mul(posW, gViewProj);
+    
+    float4 texC = mul(float4(t, 0.0f, 1.0f), texTransform);
+    dout.TexC = mul(texC, matData.MatTransform).xy;
+    
+    dout.MatIndex = matIndex;
+	
+    return dout;
+}
+
+struct PixelOut
+{
+    float4 color0 : SV_Target0;
+    float4 color1 : SV_Target1;
+};
+
+PixelOut PS(DomainOut pin)
+{
+    // Fetch the material data.
+    MaterialData matData = gMaterialData[pin.MatIndex];
+    float4 diffuseAlbedo = matData.DiffuseAlbedo;
+    float3 fresnelR0 = matData.FresnelR0;
+    float roughness = matData.Roughness;
+    uint diffuseTexIndex = matData.DiffuseMapIndex;
+    
+    diffuseAlbedo *= gDiffuseMap[diffuseTexIndex].Sample(gsamLinearWrap, pin.TexC);
+	
+#ifdef ALPHA_TEST
+	// Discard pixel if texture alpha < 0.1.  We do this test as soon 
+	// as possible in the shader so that we can potentially exit the
+	// shader early, thereby skipping the rest of the shader code.
+	clip(diffuseAlbedo.a - 0.1f);
+#endif
+
+    // Interpolating normal can unnormalize it, so renormalize it.
+    pin.NormalW = normalize(pin.NormalW);
+
+    // Vector from point being lit to eye. 
+    float3 toEyeW = gEyePosW - pin.PosW;
+    float distToEye = length(toEyeW);
+    toEyeW /= distToEye; // normalize
+
+    // Light terms.
+    float4 ambient = gAmbientLight * diffuseAlbedo;
+
+    const float shininess = 1.0f - roughness;
+    Material mat = { diffuseAlbedo, fresnelR0, shininess };
+    float3 shadowFactor = 1.0f;
+    float4 directLight = ComputeLighting(gLights, mat, pin.PosW,
+        pin.NormalW, toEyeW, shadowFactor);
+
+    float4 litColor = ambient + directLight;
+
+#ifdef FOG
+    float fogAmount = saturate((distToEye - gFogStart) / gFogRange);
+    litColor = lerp(litColor, gFogColor, fogAmount);
+#endif
+
+    // Common convention to take alpha from diffuse albedo.
+    litColor.a = diffuseAlbedo.a;
+
+    PixelOut ret;
+    ret.color0 = litColor;
+    ret.color1 = litColor;
+    return ret;
+}
