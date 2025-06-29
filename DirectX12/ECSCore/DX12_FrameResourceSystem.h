25ac17785768902d8606c7049b50efdb131755a1
kyun <psg9710@naver.com>
2025-06-30 02:58:23 +0900

    world matrix 변환 및 관리 방식 고민 진행 중

--- DirectX12/ECSCore/DX12_FrameResourceSystem.h
+++ DirectX12/ECSCore/DX12_FrameResourceSystem.h
@@ -30,6 +30,56 @@ public:
 		DX12_CommandSystem::GetInstance().FlushCommandQueue(mFrameResources[mCurrFrameResourceIndex].fenceValue);
 		mFrameResources[mCurrFrameResourceIndex].commandAllocator->Reset();
 		DX12_CommandSystem::GetInstance().ResetCommandList(mFrameResources[mCurrFrameResourceIndex].commandAllocator.Get());
+
+		{
+			// if (BaseBoundingBox)
+			// {
+			// 	BoundingBox.Center.x = BaseBoundingBox->Center.x * Scale.x + Translation.x;
+			// 	BoundingBox.Center.y = BaseBoundingBox->Center.y * Scale.y + Translation.y;
+			// 	BoundingBox.Center.z = BaseBoundingBox->Center.z * Scale.z + Translation.z;
+
+			// 	BoundingBox.Extents.x = BaseBoundingBox->Extents.x * Scale.x;
+			// 	BoundingBox.Extents.y = BaseBoundingBox->Extents.y * Scale.y;
+			// 	BoundingBox.Extents.z = BaseBoundingBox->Extents.z * Scale.z;
+			// }
+			// if (BaseBoundingSphere)
+			// {
+			// 	BoundingSphere.Center.x = BaseBoundingSphere->Center.x * Scale.x + Translation.x;
+			// 	BoundingSphere.Center.y = BaseBoundingSphere->Center.y * Scale.y + Translation.y;
+			// 	BoundingSphere.Center.z = BaseBoundingSphere->Center.z * Scale.z + Translation.z;
+			// 	BoundingSphere.Radius = BaseBoundingSphere->Radius * Scale.Length();
+			// }
+
+			// float rx = DirectX::XMConvertToRadians(Rotate.x);
+			// float ry = DirectX::XMConvertToRadians(Rotate.y);
+			// float rz = DirectX::XMConvertToRadians(Rotate.z);
+			// RotationQuat = DirectX::XMQuaternionRotationRollPitchYaw(rx, ry, rz);
+
+			// DirectX::XMMATRIX rotX = DirectX::XMMatrixRotationX(rx);
+			// DirectX::XMMATRIX rotY = DirectX::XMMatrixRotationY(ry);
+			// DirectX::XMMATRIX rotZ = DirectX::XMMatrixRotationZ(rz);
+
+			// DirectX::XMMATRIX rot = rotX * rotY * rotZ;
+			// if (useQuat)
+			// 	rot = DirectX::XMMatrixRotationQuaternion(RotationQuat);
+
+			// DirectX::XMMATRIX world
+			// 	= DirectX::XMMatrixScaling(Scale.x, Scale.y, Scale.z)
+			// 	* rot
+			// 	* DirectX::XMMatrixTranslation(Translation.x, Translation.y, Translation.z);
+			// DirectX::XMMATRIX texTransform = DirectX::XMMatrixScaling(TexScale.x, TexScale.y, TexScale.z);
+			// DirectX::XMVECTOR det = DirectX::XMMatrixDeterminant(world);
+
+			// InstanceData.World = DirectX::XMMatrixTranspose(world);
+			// InstanceData.TexTransform = DirectX::XMMatrixTranspose(texTransform);
+			// InstanceData.WorldInvTranspose = DirectX::XMMatrixInverse(&det, world);
+
+			InstanceData instanceData;
+			instanceData.World = float4x4::Identity;
+		}
+		
+		
+		mFrameResources[mCurrFrameResourceIndex].InstanceBuffer->CopyData(0, InstanceData{});
 	}
 	void EndFrame()
 	{
